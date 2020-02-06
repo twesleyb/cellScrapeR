66a30f29ee3c3b928ba80b93037f3c02128a1816
twesleyb <tyler.w.bradshaw@duke.edu>
2020-02-06 12:39:21 -0500

    add R wrapper that calls python function and gets all available datasets and saves as RData

---
+++ R/updateDatasets.R
@@ -0,0 +1,23 @@
+#!/usr/bin/env Rscript
+
+updateDatasets <- function(){
+	suppressPackageStartupMessages({
+		library(rjson)
+	})
+	# Get all links to SCell datasets.
+	here <- getwd()
+	root <- dirname(here)
+	funcdir <- file.path(root,"Py")
+	rdatdir <- file.path(root,"rdata")
+	# Call getSCellDatasets() to collect all available datasets.
+	cmd <- file.path(funcdir,"getSCellDatasets.py")
+	json_file <- system(cmd,intern=TRUE,
+			    ignore.stdout=FALSE,ignore.stderr=FALSE)
+	datasets <- fromJSON(paste(readLines(json_file), collapse=""))
+	unlink(json_file)
+	# Save to RData.
+	myfile <- gsub("json","RData",json_file)
+	saveRData(datasets,myfile)
+}
+
+updateDatasets()
