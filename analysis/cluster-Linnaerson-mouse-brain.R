48b5b888012340ac194c9a2f39620e60634be87a
twesleyb <tyler.w.bradshaw@duke.edu>
2020-02-23 13:11:38 -0500

    try clustering with kmeans

--- analysis/cluster-Linnaerson-mouse-brain.R
+++ analysis/cluster-Linnaerson-mouse-brain.R
@@ -10,6 +10,7 @@
 suppressPackageStartupMessages({
 	library(impute)
 	library(data.table)
+	library(dplyr)
 })
 
 # Functions.
@@ -27,15 +28,45 @@ rownames(adjm) <- adjm$X
 adjm$X <- NULL
 adjm <- as.matrix(adjm)
 
-# Remove genes with too many missing values.
+# Load Ensembl-Gene Symbol map.
+myfile <- file.path(downdir,'Gene_Ensembl_Map.csv')
+gene_map <- fread(myfile,drop=1,header=TRUE)
+gene_map
+
+# Load cell cluster markers.
+myfile <- file.path(downdir,'Cell_Cluster_Markers.csv')
+markers <- fread(myfile,drop=1,header=TRUE)
+
+# Collect cell markers as a named list.
+markers <- markers  %>% group_by(Cluster) %>% group_split() 
+cell_markers <- lapply(markers,function(x) unlist(strsplit(x$Genes," ")))
+names(cell_markers) <- sapply(markers,function(x) x$Cluster)
+
+# Map genes to Ensembl Ids.
+cell_markers <- lapply(cell_markers,function(x) {
+			       idx <- match(x,gene_map$Symbol)
+			       names(x) <- gene_map$Ensembl[idx]
+			       return(x)
+})
+
+cell_markers <- unlist(cell_markers)
+cell <- sapply(strsplit(names(cell_markers),"\\."),"[",1)
+gene <- sapply(strsplit(names(cell_markers),"\\."),"[",2)
+names(cell) <- gene
+cell_markers <- cell
+
+# Remove genes with too many missing values from expression data.
 percent_missing <- apply(adjm,1,function(x) sum(x==0)/length(x))
 out <- percent_missing > 0.5
 nout <- formatC(sum(out),big.mark=",")
-message(paste("Number of genes that are expressed in less than 50%",
-	      "of cell clusters:",nout))
 adjm <- adjm[!out,]
 
-# Impute the remaining missing values as MNAR.
+# Status report.
+message(paste("Number of genes that are expressed in less than 50%",
+	      "of cell clusters:",nout))
+message(paste("Number of remaining genes:",nrow(adjm)))
+
+# Impute the remaining missing values as MNAR with KNN.
 adjm <- impute.knn(adjm)$data
 
 # Perform bicor for a subset of genes.
@@ -57,8 +88,22 @@ k <- ncol(adjm)
 nstart <- 1
 iter.max <- 10
 alg <- c("Hartigan-Wong", "Lloyd", "Forgy","MacQueen")[1]
-partition <- kmeans(cormat, centers=k, iter.max, nstart, algorithm=alg)
+clusters <- kmeans(cormat, centers=k, iter.max, nstart, algorithm=alg)
 
-sum(table(partition$cluster))
+# Extract clusters.
+partition <- clusters$cluster
+names(partition) <- colnames(cormat)
 
+# Split into list of cell clusters.
+cell_clusters <- split(partition,partition)
+names(cell_clusters) <- paste0("C",names(cell_clusters))
+
+# All cell markers.
+all_markers <- unlist(lapply(cell_markers,names),use.names=FALSE)
+
+# How many cell markers were clustered?
+sum(names(partition) %in% all_markers)
+
+idx <- which(sapply(cell_clusters,function(x) sum(names(x) %in% all_markers))==2)
+subclust <- cell_clusters[idx]
 
--- analysis/scrape-Linnaerson-mouse-brain.py
+++ analysis/scrape-Linnaerson-mouse-brain.py
@@ -17,14 +17,15 @@ ds = loompy.connect(myfile,mode='r',validate=False)
 
 # Scrape all of the data.
 clusters = list(ds.ca['ClusterName'])
-genes = ds.ra['Accession']
+gene_ids = ds.ra['Accession']
+genes = ds.ra['Gene']
 
 # Scrape cell markers.
 cell_markers = ds.ca['MarkerGenes']
 
 # List comprehension to get expression data for every gene.
 # Not sure why it has to be done like this...
-data = [ds[ds.ra.Accession == gene,:][0] for gene in genes]
+data = [ds[ds.ra.Accession == gene_id,:][0] for gene_id in gene_ids]
 
 # Create a df and write to csv.
 df = pd.DataFrame(data,index=genes,columns=clusters)
@@ -32,6 +33,11 @@ myfile = join(downdir,'Expression_Matrix.csv')
 df.to_csv(myfile)
 
 # Create df of cell markers and write to csv.
-markers = pd.DataFrame(cell_markers,columns=['Genes'],index=clusters)
+markers = pd.DataFrame({'Cluster':clusters,'Genes':cell_markers})
 myfile = join(downdir,'Cell_Cluster_Markers.csv')
 markers.to_csv(myfile)
+
+# Create a df of ensembl-gene name annotations.
+gene_map = pd.DataFrame({'Symbol':genes,'Ensembl':gene_ids})
+myfile = join(downdir,'Gene_Ensembl_Map.csv')
+gene_map.to_csv(myfile)
